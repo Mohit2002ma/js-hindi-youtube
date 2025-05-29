43619d625f068db58bb56a5a33afbe790157ab4b
Mohit mishra <mishramohit1111111@gmail.com>
2025-05-29 06:41:02 +0000

    Your commit message

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId =144553
+let accountEmail = "mohit@google.com"
+var accountPassword = "12345"
+accountCity ="indore"
+let accountState;  
+
+//accountId =2// not allowed
+
+accountEmail ="hc@hc.com"
+accountPassword ="21212121"
+accountCity ="sagar"
+
+console.log(accountId);
+console.table ([ accountId, accountEmail, accountPassword, accountCity ,accountState])
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
